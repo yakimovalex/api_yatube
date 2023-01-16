bbaf3fae0fde4d9f5a149bea3da2490496ab8da3
Aleksei Iakimov <yakimovizh@gmail.com>
2023-01-16 08:42:23 +0400

    2301160842

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,9 @@
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
-from posts.models import Group, Post
 from rest_framework import viewsets
 
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
-
+from posts.models import Group, Post
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
